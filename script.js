e9defbf5a80821e95a3fff99038bba77e7d8b7ee
Kushagra Vardhan <kushagravardhan619@gmail.com>
2024-06-25 11:49:59 +0530

    1st Upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization:  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODJlNGU2OGUxYzYwMmE4MmEwYjJmMWQ3YWE5MjEwYyIsIm5iZiI6MTcxOTE0OTM3My40MDE1NzIsInN1YiI6IjY2NTgxYTcxOTdiN2M1MjQ3NTc0ZGQ4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yKoT45QvzSkKkvaa6xF29C_LgWLftHZI5W_2oPPcOVo'
+    }
+};
+
+const nameField = document.querySelector("#name");
+const genreField = document.querySelector("#genre");
+const releaseDateField = document.querySelector("#release-date");
+const popularityField = document.querySelector("#popularity");
+const runtimeField = document.querySelector("#run-time");
+const searchField = document.querySelector(".input_field");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", getMovieValue);
+
+
+
+
+
+
+const fetchResults = async (movieId) => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}`, options);
+    const data = await response.json();
+    console.log(data);
+    let genre = data.genres[0].name;
+    let releaseDate = data.release_date;
+    let Popularity = data.popularity;
+    let runTime = data.runtime;
+
+    updateData(genre,releaseDate,Popularity,runTime,movieName );
+    
+}
+
+function getMovieValue (evt){
+    evt.preventDefault();
+
+    movieName = searchField.value;
+    console.log(movieName)
+    searchMovie(movieName);
+    
+}
+
+
+
+// let movieName = "Star Wars"
+// Making a function to fetch movie name by its id
+
+
+function updateData(genre,releaseDate,Popularity,runTime,movieName ){
+    //updating all the data
+    nameField.innerText = movieName;
+    genreField.innerText = genre;
+    releaseDateField.innerText = releaseDate;
+    popularityField.innerText = Popularity;
+    runtimeField.innerText = runTime;
+    console.log(movieName);
+}
+
+
+
+const searchMovie = async (movieName) => {
+    
+        // Use encodeURIComponent to properly format the movieName for the URL
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(movieName)}`, options);
+        const data = await response.json();
+        // console.log(data);
+    if (data.results && data.results.length > 0) {
+        let finalData = data.results[0].id; // Return the ID of the first matching movie
+        console.log(finalData);
+        fetchResults(finalData);
+        return finalData;
+        
+    } else {
+        throw new Error('Movie not found');
+    }
+    
+};
+
+
+
+
